9451c745ce44cbfdf62739f665cb132c728244f4
Rayane-Souza <rayannesilvasouza@outlook.com>
2024-11-21 16:44:25 -0300

    commit

--- servpro/vite.config.js
+++ servpro/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: './servpro',  // Garante que a raiz do Vite seja a pasta "servpro"
+  build: {
+    outDir: './dist',  // Define a pasta de saída para os arquivos de build
+  },
 })
